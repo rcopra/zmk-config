cc794204bf443e5b777feb5a404c0160af7ca1df
rcopra <rickcopra@gmail.com>
2024-12-12 12:59:26 +0100

    added temper

--- config/extra_keys.h
+++ config/extra_keys.h
@@ -2,44 +2,43 @@
 // the macros defined in this file to one or more keys before sourcing this file.
 
 /* left of left half */
-#if !defined X_LT  // top row, left
-    #define X_LT
+#if !defined X_LT // top row, left
+#define X_LT
 #endif
-#if !defined X_LM  // middle row, left
-    #define X_LM
+#if !defined X_LM // middle row, left
+#define X_LM
 #endif
-#if !defined X_LB  // bottom row, left
-    #define X_LB
+#if !defined X_LB // bottom row, left
+#define X_LB
 #endif
-#if !defined X_LH  // thumb row, left
-    #define X_LH
+#if !defined X_LH // thumb row, left
+#define X_LH
 #endif
 
 /* between left and right half */
-#if !defined X_MT  // top row, middle
-    #define X_MT
+#if !defined X_MT // top row, middle
+#define X_MT
 #endif
-#if !defined X_MM  // middle row, middle
-    #define X_MM
+#if !defined X_MM // middle row, middle
+#define X_MM
 #endif
-#if !defined X_MB  // bottom row, middle
-    #define X_MB
+#if !defined X_MB // bottom row, middle
+#define X_MB
 #endif
-#if !defined X_MH  // thumb row, middle
-    #define X_MH
+#if !defined X_MH // thumb row, middle
+#define X_MH
 #endif
 
 /* right of right half */
-#if !defined X_RT  // top row, right
-    #define X_RT
+#if !defined X_RT // top row, right
+#define X_RT
 #endif
-#if !defined X_RM  // middle row, right
-    #define X_RM
+#if !defined X_RM // middle row, right
+#define X_RM
 #endif
-#if !defined X_RB  // bottom row, right
-    #define X_RB
+#if !defined X_RB // bottom row, right
+#define X_RB
 #endif
-#if !defined X_RH  // thumb row, right
-    #define X_RH
+#if !defined X_RH // thumb row, right
+#define X_RH
 #endif
-
